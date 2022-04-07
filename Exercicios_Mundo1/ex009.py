9e28088a5cde82faf3618dfa0538c80cc8c77a8f
Rogério Filho <rogeriofilho63@gmail.com>
2022-04-06 21:35:28 -0300

    Exercicio - Area e Volume

---
+++ Exercicios_Mundo1/ex009.py
@@ -0,0 +1,3 @@
+largura = float(input('Qual a largura da parede? '))
+altura = float(input('Qual a altura da parede? '))
+print(f'A area da sua parede é de {altura*largura}m² e o volume de tinta gasto foi {(altura*largura)/2}L')
