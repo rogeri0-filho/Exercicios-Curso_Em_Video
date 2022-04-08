aa80cf5e67e68dd39eb633661b3578161f4025b4
Rogério Filho <rogeriofilho63@gmail.com>
2022-04-08 16:56:16 -0300

    Exercicio - Analise de Triângulo v1.0

---
+++ Exercicios_Mundo1/ex033.py
@@ -0,0 +1,8 @@
+s1 = float(input('Digite o valor do primeiro segmento: '))
+s2 = float(input('Digite o valor do segundo segmento: '))
+s3 = float(input('Digite o valor do terceiro segmento: '))
+
+if s1 < s2 + s3 and s2 < s1 + s3 and s3 < s1 + s2:
+    print('Os segmentos de reta acima formam um triângulo!')
+else:
+    print('Os segmentos de reta não formam um triângulo!')
