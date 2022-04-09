a86b8566d702599cf589c3989e0ca79878f47e13
rogeri0-filho <rogeriofilho63@gmail.com>
2022-04-09 18:16:07 -0300

    Exercicio - Soma de Pares

---
+++ Exercicios_Mundo2/ex048.py
@@ -0,0 +1,10 @@
+soma = 0
+cont = 0
+
+for c in range(1, 7):
+    n = int(input(f'Digite o {c}º valor: '))
+    if n % 2 == 0:
+        soma += n
+        cont += 1
+
+print(f'Você informou {cont} PARES e a soma é: {soma}!')
