5e8159c3bbc18e81ba4db8d05656a7fd97cacc99
rogeri0-filho <rogeriofilho63@gmail.com>
2022-04-13 14:04:25 -0300

    Exercicio - Número por Extenso

---
+++ Exercicios_Mundo3/ex070.py
@@ -0,0 +1,11 @@
+tulpla = ('zero', 'um', 'dois', 'tres', 'quatro', 'cinco', 'seis', 'sete', 'oito',
+         'nove', 'dez', 'onze', 'doze', 'treze', 'quatorze', 'quinze',
+         'dezesseis', 'dezessete', 'dezoito', 'dezenove', 'vinte')
+
+while True:
+    n = int(input('Digite um número entre 0 e 20: '))
+    if 0 <= n <= 20:
+        break
+    print('Valor inválido, tente novamente. ', end='')
+
+print(f'Você digitou o número {tulpla[n]}.')
