d2408a4ed9efbbaa2dc5364ab781fabf2ceca527
rogeri0-filho <rogeriofilho63@gmail.com>
2022-04-12 23:46:50 -0300

    Exercicio - Vários Números Com Flag

---
+++ Exercicios_Mundo2/ex064.py
@@ -0,0 +1,11 @@
+cont = soma = 0
+
+while True:
+    n = int(input('Digite um valor (999 para parar): '))
+
+    if n == 999:
+        break
+    cont += 1
+    soma += n
+
+print(f'A soma vale {soma} e {cont} números foram digitados!')
