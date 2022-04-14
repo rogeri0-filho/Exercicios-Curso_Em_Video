d8b07bbf2b14661a8fa3e1db1e47932a44ab317c
rogeri0-filho <rogeriofilho63@gmail.com>
2022-04-14 15:12:54 -0300

    Exercicio - Lista Ordenada Sem Repetições

---
+++ Exercicios_Mundo3/ex078.py
@@ -0,0 +1,19 @@
+lista = list()
+
+for contador in range(0, 5):
+    entrada = int(input('Insira um valor: '))
+
+    if contador == 0 or entrada > lista[-1]:
+        lista.append(entrada)
+
+    else:
+        posição = 0
+
+        while posição < len(lista):
+            if entrada <= lista[posição]:
+                lista.insert(posição, entrada)
+                break
+            
+            posição += 1
+print('--' * 30)
+print(f'Os valores digitados foram: {lista}!')
