3720bd9610e030dd8511eefa380482d2ccf25e50
rogeri0-filho <rogeriofilho63@gmail.com>
2022-04-17 18:07:28 -0300

    Exercicio - Função que Calcula Área

---
+++ Exercicios_Mundo3/ex094.py
@@ -0,0 +1,12 @@
+def area(larg, comp):
+    a = larg * larg
+    print(f'A area de um terreno {larg} x {comp} é : {a}m²')
+
+
+altura = float(input('Digite a largura: m²'))
+largura = float(input('Digite o comprimento: m²'))
+
+area(altura, largura)
+
+
+
