da26d9f91fc98a67c252afcdf76b90781d29e92a
rogeri0-filho <rogeriofilho63@gmail.com>
2022-09-26 10:44:16 -0300

    Exercício – Formatando Moedas em Python

--- Exercicios_Mundo3/ex106/teste_a.py
+++ Exercicios_Mundo3/ex106/teste_a.py
@@ -5,3 +5,4 @@ print(f' A Metade de R${p} é {conversoesA.moeda(conversoesA.metade(p))}')
 print(f' O Dobro de R${p} é {conversoesA.moeda(conversoesA.dobro(p))}')
 print(f' O Valor R${p} com desconto de 10% é {conversoesA.moeda(conversoesA.desconto(p, 10))}')
 print(f' O Valor R${p} com acrescimo de 10% é {conversoesA.moeda(conversoesA.acrescimo(p, 10))}')
+
