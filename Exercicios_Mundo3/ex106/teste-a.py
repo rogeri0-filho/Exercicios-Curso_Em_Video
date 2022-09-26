6cfbdf813040e8df5b9acf1614ccbd29fe1ae112
rogeri0-filho <rogeriofilho63@gmail.com>
2022-09-26 10:42:47 -0300

    teste

--- Exercicios_Mundo3/ex106/teste-a.py
+++
@@ -1,7 +0,0 @@
-import conversoesA
-
-p = float(input('Digite um valor R$'))
-print(f' A Metade de R${p} é {conversoesA.moeda(conversoesA.metade(p))}')
-print(f' O Dobro de R${p} é {conversoesA.moeda(conversoesA.dobro(p))}')
-print(f' O Valor R${p} com desconto de 10% é {conversoesA.moeda(conversoesA.desconto(p, 10))}')
-print(f' O Valor R${p} com acrescimo de 10% é {conversoesA.moeda(conversoesA.acrescimo(p, 10))}')
