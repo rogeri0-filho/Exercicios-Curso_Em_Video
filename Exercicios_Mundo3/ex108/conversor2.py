c589ae3135a7fc987c313fdc6060d7996358c303
rogeri0-filho <rogeriofilho63@gmail.com>
2022-09-27 17:58:26 -0300

    Exercício – Reduzindo ainda mais seu programa

---
+++ Exercicios_Mundo3/ex108/conversor2.py
@@ -0,0 +1,35 @@
+def dobro(preço = 0, formatacao = False):
+    resposta = preço * 2
+    return resposta if formatacao is False else moeda(resposta)
+    
+
+def metade(preço = 0, formatacao = False):
+    resposta = preço / 2
+    return resposta if formatacao is False else moeda(resposta)
+
+
+def acrescimo(preço = 0, taxa = 0, formatacao = False):
+    resposta = f'R${preço + (preço * (taxa/100)):.2f}'.replace('.', ',')
+    return resposta if formatacao is False else moeda(resposta)
+    
+    
+def desconto(preço = 0, taxa = 0, formatacao = False):
+    resposta = f'R${preço - (preço * (taxa/100)):.2f}'.replace('.', ',')
+    return resposta if formatacao is False else moeda(resposta)
+
+
+def moeda(preço = 0):
+    resposta = f'R${preço:.2f}'.replace('.', ',') #Subistitui os '.' por ',' como seprarador das casas decimais
+    return resposta
+    
+    
+def resumo(preço = 0, taxa_aument = 10, taxa_redu = 5):
+    print('-' * 30)
+    print('RESUMO DA CONVERSÃO'.center(30))
+    print('-' * 30)
+    print(f'Valor Analisado: \t{moeda(preço)}')
+    print(f'Dobro do Valor: \t{dobro(preço, True)}')
+    print(f'Metade do Valor: \t{metade(preço, True)}')
+    print(f'Acrscimo (10%-padrão): \t{acrescimo(preço, True)}')
+    print(f'Desconto (5%-padrão): \t{desconto(preço, True)}')
+    print('-' * 30)
---
+++ Exercicios_Mundo3/ex108/teste_c.py
@@ -0,0 +1,4 @@
+import conversor2
+
+p = float(input('Digite um valor R$'))
+conversor2.resumo(p)
